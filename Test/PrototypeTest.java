ec252c4e914fd63e79dfae2b8443dd666a74f306
mooooon333 <742932097@qq.com>
2019-11-11 14:32:54 +0800

    提交

--- Test/PrototypeTest.java
+++ Test/PrototypeTest.java
@@ -1,14 +1,18 @@
 import MyFarm.animals.Dog;
 import MyFarm.animals.Pig;
 
+/**
+ * 原型模式测试
+ * 通过
+ */
 public class PrototypeTest {
     public static void main(String[] args) {
-
 
         Dog dog = new Dog();
         dog.setName("原型dog");
         Dog dog1 = (Dog) dog.clone();
         System.out.println("创建的原型dog名字：" + dog.getName());
         System.out.println("克隆下来的dog名字：" + dog1.getName());
+
     }
 }
